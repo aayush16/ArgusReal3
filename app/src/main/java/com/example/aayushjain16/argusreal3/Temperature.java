1d58195c5c1d5ef91c1ddb171bcdca0ebc9e7dd7
Tony Tran <tbtran96@vt.edu>
2018-03-13 05:24:38 -0400

    Real time data with fake data

--- app/src/main/java/com/example/aayushjain16/argusreal3/Temperature.java
+++ app/src/main/java/com/example/aayushjain16/argusreal3/Temperature.java
@@ -1,16 +1,77 @@
 package com.example.aayushjain16.argusreal3;
 
+
+import java.util.Random;
+
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.Viewport;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+
 public class Temperature extends AppCompatActivity {
+
+    private LineGraphSeries<DataPoint> series;
+    private int lastX = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temperature);
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        series = new LineGraphSeries<DataPoint>();
+        graph.addSeries(series);
+        graph.getGridLabelRenderer().setHorizontalLabelsVisible(false);
+        graph.getGridLabelRenderer().setVerticalAxisTitle("Temperature °F");
+        Viewport viewport = graph.getViewport();
+        viewport.setXAxisBoundsManual(true);
+        viewport.setMinX(0);
+        viewport.setMaxX(10);
+        viewport.setYAxisBoundsManual(true);
+        viewport.setMinY(0);
+        viewport.setMaxY(100);
+        viewport.setScrollable(true);
+
+
+        graph.addSeries(series);
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            addEntry();
+                        }
+                    });
+
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        // manage error ...
+                    }
+                }
+            }
+        }).start();
+    }
+
+    // add random data to graph
+    private void addEntry() {
+        Random r = new Random();
+        series.appendData(new DataPoint(lastX++, r.nextInt(101)), true, 10);
     }
 
     public void goBack(View view){
